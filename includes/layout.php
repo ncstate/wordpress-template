355a13fd230366e58a3092ace859f4c4f1c001a1
Scott Thompson <csthomp2@ncsu.edu>
2015-07-28 09:06:33 -0400

    Old code cleanup

--- includes/layout.php
+++ includes/layout.php
@@ -2,18 +2,6 @@
 
 $theme = get_option('ncstate_theme');
 
-$layout = $theme['opt-layout'];
-
-$fluid="";
-if($layout=="left") {
-	$fluid = "-fluid";
-}
-
-$brick="2x1";
-if($theme['opt-brick']=='2x2') {
-	$brick="2x2";
-}
-
 // Default Bootstrap Breakpoints
 // Used for <picture> elements
 $lg_breakpoint = "1200px";
