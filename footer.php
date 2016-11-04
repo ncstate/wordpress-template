e66e1451b2bd1dc235d27c308c0c2dffd76649dc
Scott Thompson <csthomp2@ncsu.edu>
2016-11-04 13:46:15 -0400

    Update for Google Analytics tracking code to be included

--- footer.php
+++ footer.php
@@ -53,11 +53,18 @@
 		</div>
 	</footer>
 
-	<?php 
-			if ($theme['opt-tracking-code']!=NULL) :
-				echo $theme['opt-tracking-code'];
-		  	endif; 
-	?>
+	<?php if (ot_get_option('ga_id')) : ?>
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', <?php echo '\'' . ot_get_option('ga_id') . '\''; ?>, 'auto');
+	ga('send', 'pageview');
+
+	</script>
+	<?php endif; ?>
 	<?php wp_footer(); ?> 
 	
 	</div> <!--mobile-nav-slide-out -->
